59da365d26de2c6fb71002a30d61c8fc1a77d33e
gonzalez.juan.domingo <gonzalez.juan.domingo@29dd44f4-06a0-1f5f-19ae-3cdbe9c1cebb>
2011-10-30 18:49:33 +0000

    Imagenes y algunas clases

---
+++ DS-UNS/src/sanidadApp/Core/sanidadAppCore.java
@@ -0,0 +1,26 @@
+package sanidadApp.Core;
+
+import sMySQLappTemplate.Core.AppCoreTemplate;
+
+public class sanidadAppCore extends AppCoreTemplate 
+{
+
+	@Override
+	protected void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initFeatures() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initGUI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
