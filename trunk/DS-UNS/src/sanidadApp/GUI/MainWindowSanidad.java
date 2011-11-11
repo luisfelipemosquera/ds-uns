e9d400aed7148460e1d1cae7dfb007b8f0ac1d23
gonzalez.juan.domingo <gonzalez.juan.domingo@29dd44f4-06a0-1f5f-19ae-3cdbe9c1cebb>
2011-11-11 12:39:03 +0000

    agregada columna de notificaciones base

--- trunk/DS-UNS/src/sanidadApp/GUI/MainWindowSanidad.java
+++ trunk/DS-UNS/src/sanidadApp/GUI/MainWindowSanidad.java
@@ -3,13 +3,14 @@ package sanidadApp.GUI;
 import sMySQLappTemplate.GUI.MyBorder;
 import sMySQLappTemplate.GUI.MyButtonPanel;
 import sMySQLappTemplate.GUI.MyMenuBar;
+import sMySQLappTemplate.GUI.MyNotification;
 import sMySQLappTemplate.GUI.MyTable;
 import sanidadApp.Core.sanidadAppCore;
 
 import java.awt.*;
+
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("serial")
 public class MainWindowSanidad extends sMySQLappTemplate.GUI.MainWindowTemplate
@@ -19,6 +20,9 @@ public class MainWindowSanidad extends sMySQLappTemplate.GUI.MainWindowTemplate
 	public JMenuBar mainMenuBar;
 	
 	public sanidadAppCore appCore;
+	
+	protected JScrollPane scrollPrincipal;
+	protected MyTable tablaPrincipal;
 	
 	public MainWindowSanidad(sanidadAppCore AppCore)
 	{
@@ -38,22 +42,45 @@ public class MainWindowSanidad extends sMySQLappTemplate.GUI.MainWindowTemplate
 		mainMenuBar = new MyMenuBar();		
 		this.setJMenuBar(mainMenuBar);
 		
-		// Panel de Seleccion de Reservas	
-		JPanel applicationsPane = new JPanel(new BorderLayout());
-		applicationsPane.setPreferredSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
-		applicationsPane.setBorder(new MyBorder(4,"Reservas del Día"));
+		// Panel de Seleccion de turnos	
+		JPanel leftPrincipalPane = new JPanel(new BorderLayout());
+		leftPrincipalPane.setPreferredSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
+		leftPrincipalPane.setBorder(new MyBorder(4,"Turnos"));
 		
-		this.getContentPane().add(applicationsPane);
+		this.getContentPane().add(leftPrincipalPane, JSplitPane.LEFT);
+		
+		scrollPrincipal = new JScrollPane();
+		leftPrincipalPane.add(scrollPrincipal);
+		
+		// Panel de Notificaciones
+		
+		JPanel rightPrincipalPane = new JPanel();
+		BoxLayout layout = new BoxLayout(rightPrincipalPane, BoxLayout.Y_AXIS);
+		rightPrincipalPane.setLayout(layout);
+		rightPrincipalPane.setBorder(new MyBorder(4,"Notificaciones"));
+		
+		this.getContentPane().add(rightPrincipalPane, JSplitPane.RIGHT);
+		
+		// TESTING
+		rightPrincipalPane.add(new MyNotification("Se elimino un turno reservado."));
+		rightPrincipalPane.add(new MyNotification("Se elimino un turno reservado."));
+		rightPrincipalPane.add(new MyNotification("Se elimino un turno reservado."));
 		
 		// Panel de Herramientos de Reservas
 		tools = new MyButtonPanel();
 		
-		applicationsPane.add(tools, BorderLayout.SOUTH);
+		leftPrincipalPane.add(tools, BorderLayout.SOUTH);
 		
 		this.setVisible(true);
 	}
 
-	public int getTablaReservasSelectedRow(){
-		return 0;
+	public int getTablaPrincipalSelectedRow(){
+		return tablaPrincipal.getSelectedRow();
+	}
+	
+	public void setTablaPrincipal (DefaultTableModel table)
+	{
+		tablaPrincipal = new MyTable(table);
+		scrollPrincipal.setViewportView(tablaPrincipal);
 	}
 }
